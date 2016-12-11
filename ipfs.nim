67b38d2380b7a5a6cd861f998b688e5994c99c72
Khaled Karam <mohamedk@greenitglobe.com>
2016-12-11 15:31:26 +0400

    refactor ipfs

--- ipfs.nim
+++ ipfs.nim
@@ -6,64 +6,77 @@ import strutils
 import tables
 import httpclient, json
 import typetraits
-import pymod
 
 
-proc upload*(filename: string, msgpack:bool) : string {.exportpy.} =
+let client = newHttpClient()
+var
+  hashSeq: seq[string]
+  hashaMap: seq[string]
+  hashbMap: seq[string]
+
+proc addToIpfs(input: string) : string =
+    var data = newMultipartData()
+    data["path"] = (input, "text/html", input)
+    var output = client.postContent("http://127.0.0.1:5001/api/v0/add", multipart=data)
+    var hash = parseJson(output)["Hash"]
+    return $hash
+
+proc hash(chunk: cstring, len: int) : string =
     var
-        hashamap = ""
-        hashbmap = ""
-    let client = newHttpClient()
-    const blockSize: int = 1024 * 1024
-    var
-        c1: MD5Context
-        d1: MD5Digest
-        c2: MD5Context
-        d2: MD5Digest
-        f: File
-        bytesRead: int = 0
-        buffer: array[blockSize, char]
-        byteTotal: int = 0
+        d: MD5Digest
+        c: MD5Context
+    c.md5Init()
+    c.md5Update(chunk, len)
+    c.md5Final(d)
+    return $d
 
-    # read chunk of file, calling update until all bytes have been read
-    try:
-        f = open(filename)
+proc hashChainToSeq(hashChain: string) : seq =
+  hashSeq = @[]
+  for hash in hashChain.split(","):
+      hashSeq.add(hash)
+  return hashSeq
 
-        md5Init(c1)
-        bytesRead = f.readBuffer(buffer.addr, blockSize)
+proc buildHashChain(chain: string, hash: string) : string =
+  return if chain == nil: hash else: chain & "," & hash
 
-        while bytesRead > 0:
-            md5Init(c2)
-            byteTotal += bytesRead
-            md5Update(c1, buffer, bytesRead)
-            md5Update(c2, buffer, bytesRead)
-            md5Final(c2, d2)
-            var hash = compress($d2)
-            var data = newMultipartData()
-            data["path"] = (hash, "text/html", hash)
-            var output = client.postContent("http://127.0.0.1:5001/api/v0/add", multipart=data)
-            var hashb = parseJson(output)["Hash"]
-            bytesRead = f.readBuffer(buffer.addr, blockSize)
-            if hashamap == "":
-                hashamap = hashamap & $d2
-                hashbmap = hashbmap & $hashb
-            else:
-                hashamap = hashamap & "," & $d2
-                hashbmap = hashbmap & "," & $hashb
-        md5Final(c1, d1)
-    except IOError:
-        echo("File not found.")
-    finally:
-        if f != nil:
-            f.close()
-    let hashes = "$#&$#&$#" % [hashamap, hashbmap, $d1]
-    return hashes
+proc upload*(filename: string) : (seq[string], seq[string], string) =
+  const blockSize: int = 1024 * 1024
+  var
+      c1: MD5Context
+      d1: MD5Digest
+      f: File
+      bytesRead: int = 0
+      buffer: array[blockSize, char]
+      byteTotal: int = 0
 
-# if paramCount() > 0:
-#     let arguments = commandLineParams()
-#     echo("MD5: ", calculateMD5Incremental(arguments[0]))
-# else:
-#     echo("Must pass filename.")
-#     quit(-1)
+  try:
+    hashaMap = @[]
+    hashbMap = @[]
+    f = open(filename)
+    md5Init(c1)  # Initialize context to hash whole file
+    bytesRead = f.readBuffer(buffer.addr, blockSize)
 
-initPyModule("encrypt", calculateMD5Incremental)
+    while bytesRead > 0:
+      # Reading chunks, hash, addToIpfs
+      byteTotal += bytesRead
+      var hasha = hash(buffer, bytesRead)  # MD5 hash
+      c1.md5Update(buffer, bytesRead)
+      var hashb = addToIpfs(compress(hasha))  # IPFS hash to the compressed md5
+      bytesRead = f.readBuffer(buffer.addr, blockSize)
+      hashaMap.add($hasha)
+      hashbMap.add($hashb)
+    c1.md5Final(d1)  # Getting the hash of the whole file
+  except IOError:
+      echo("File not found.")
+  finally:
+      if f != nil:
+          f.close()
+  return (hashaMap, hashbMap, $d1)
+
+if paramCount() > 0:
+    let arguments = commandLineParams()
+    echo upload(arguments[0])
+    # echo("MD5: ", upload(arguments[0]))
+else:
+    echo("Must pass filename.")
+    quit(-1)
